82da0809678cf0b75af39dc319e5b91dc8dce021
Christina Landvytovych <Crofty.land@gmail.com>
2018-09-18 18:23:20 +0300

    adding files into directories

---
+++ day 1:drumKit/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Crofty on 7/7/18.
+ */
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform')return;
+    e.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+};
+
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
